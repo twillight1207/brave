432b28ce67e612d3cac2aa3fd00f93609ddd5d08
twillight1207 <73545327+twillight1207@users.noreply.github.com>
2021-09-01 12:03:00 +0900

    first_building_animation_trial1

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 const flick = document.querySelectorAll(".floor span");
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
-  }
+// random 숫자 리스트 만들기
+function genRandomNum() {
+  const num1 = Math.floor(Math.random() * 40);
+  const num2 = Math.floor(Math.random() * 40);
+  const num3 = Math.floor(Math.random() * 40);
+  const randomNumbers = [num1, num2, num3];
+  return randomNumbers;
+}
 
-  
+
+// span list 중 선택
+function chooseAnimated() {
+  span1.classList.add("animatedWindow");
+  span2.classList.add("animatedWindow");
+  span3.classList.add("animatedWindow");
+}
+
+// classname 제거
+function removeAnimated() {
+  span1.classList.remove("animatedWindow");
+  span2.classList.remove("animatedWindow");
+  span3.classList.remove("animatedWindow");
+}
+
+
+
+// 1초마다 함수 반복
+const randomNumbers = genRandomNum();
+const span1 = flick[randomNumbers[0]];
+const span2 = flick[randomNumbers[1]];
+const span3 = flick[randomNumbers[2]];
+setInterval(chooseAnimated, 1000);
+setTimeout(function run() {
+  removeAnimated();
+  setTimeout(run, 1000);
+}, 1000);
